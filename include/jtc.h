23fed12af5139953d8999dbb2c1a94d833482ee6
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-03-25 11:09:05 +0100

    initial commit

---
+++ include/jtc.h
@@ -0,0 +1,11 @@
+#ifndef POLYGON_H
+#define POLYGON_H
+
+#include "point.h"
+#include <vector>
+
+namespace jtc {
+std::vector<Point> polygon(std::vector<Point>& points);
+}
+
+#endif /* POLYGON_H */
